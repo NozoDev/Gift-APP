24e8d4b53f446d001c69964c06e1efa1734ac342
NozoDev <kevinroj326@gmail.com>
2024-03-21 16:16:57 -0500

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const GifExpertApp = () => {
+  return (
+    <div>
+        {/* Titulo */}
+      <h1>Gif</h1>
+
+      {/* Input */}
+
+      {/* Listado de gift */}
+            {/* Gif item */}
+    </div>
+  )
+}
+
+export default GifExpertApp
